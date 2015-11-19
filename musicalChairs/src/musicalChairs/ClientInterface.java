b5c879e5bdb102b4041a3b99da2dbbb8baf88581
nogna <nogna@hotmail.se>
2015-11-19 21:00:12 +0100

    fixade print

--- musicalChairs/src/musicalChairs/ClientInterface.java
+++ musicalChairs/src/musicalChairs/ClientInterface.java
@@ -3,51 +3,46 @@ package musicalChairs;
 import java.util.Scanner;
 
 public class ClientInterface {
-	
-	
-	/**
-	 * 
-	 * @param choices what we get from the server
-	 * @return the client request
-	 */
 
-	public static String getRequest(String[] choices) {
-		int tmp_req;
-		Scanner sc = new Scanner(System.in);
-		do{
-			CheckIfInt(sc, choices.length);
-			tmp_req=sc.nextInt();
-			
-		} while (validInt(tmp_req, choices.length));
-		
-		return	choices[tmp_req];
-	}
-	
-	/**
-	 * 
-	 * @param Input from the player
-	 * @param length the length of the choice array
-	 */
-	
+    /**
+     *
+     * @param choices what we get from the server
+     * @return the client request
+     */
+    public static String getRequest(String[] choices) {
+        int tmp_req;
+        Scanner sc = new Scanner(System.in);
+        do {
+            CheckIfInt(sc, choices.length);
+            tmp_req = sc.nextInt();
+
+        } while (validInt(tmp_req, choices.length));
+
+        return choices[tmp_req];
+    }
+
+    /**
+     *
+     * @param Input from the player
+     * @param length the length of the choice array
+     */
     private static void CheckIfInt(Scanner Input, int length) { // Bör fixas bättre
         while (!Input.hasNextInt()) {
             System.out.println("Plz a number between 1 and " + length + ": ");
             Input.next();
         }
     }
-	
+
     private static boolean validInt(int tmp_req, int length) { //Bör fixas bättre
-    	return (tmp_req <= 1 && tmp_req >= length);
-	}
-
-
+        return (tmp_req <= 1 && tmp_req >= length);
+    }
 
     /*
     Printthe requests that are availiblefor the client.
-    */
-	public static void printChoices(String[] choices) {
-	           for (int i = 0; i < choices.length; i++) {
-                       System.out.println(choices[i].toString()); //Kanske måste göra egen toString
-            }
-	}
+     */
+    public static void printChoices(String[] choices) {
+        for (int i = 0; i < choices.length; i++) {
+            System.out.println(i +": "+ choices[i].toString()); //Kanske måste göra egen toString
+        }
+    }
 }
