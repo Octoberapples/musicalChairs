e8479340ec6ff86cd94223d32d75be1976e9ade0
Octoberapples <linneamd@gmail.com>
2015-11-19 18:26:13 +0100

    Create ServerResponseRepository.java

---
+++ ServerResponseRepository.java
@@ -0,0 +1,39 @@
+package musicalChairs;
+
+public class ServerResponseRepository {
+
+	public String[] getResponseChoices(String state) {
+		switch (state) {
+		case "RequestToJoin":
+			return getChoicesRequestToJoin();
+
+		case "InPlayerQueue":
+			return getChoicesInPlayerQueue();
+		case "InGame":
+			return getChoicesInGame();
+		}
+		String[] x = new String[0];
+		return x;
+	}
+
+	private String[] getChoicesInGame() {
+		String[] response = new String[2];
+		response[0] = ("Sit Down");
+		response[1] = ("Leave Game");
+		return response;
+	}
+
+	private String[] getChoicesInPlayerQueue() {
+		String[] response = new String[2];
+		response[0] = ("Force start");
+		response[1] = ("Leave Game");
+		return response;
+	}
+
+	private String[] getChoicesRequestToJoin() {
+		String[] response = new String[1];
+		response[0] = ("Join Game");
+		return response;
+	}
+
+}
