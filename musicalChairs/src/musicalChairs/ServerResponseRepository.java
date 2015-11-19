f385221f49a2af59d8983cc2e4fa5bdc05621a01
nogna <nogna@hotmail.se>
2015-11-19 21:00:37 +0100

    tog bort "join game"

--- musicalChairs/src/musicalChairs/ServerResponseRepository.java
+++ musicalChairs/src/musicalChairs/ServerResponseRepository.java
@@ -2,33 +2,30 @@ package musicalChairs;
 
 public class ServerResponseRepository {
 
-	public String[] getResponseChoices(String state) {
-		switch (state) {
+    public String[] getResponseChoices(String state) {
+        switch (state) {
 
+            case "InPlayerQueue":
+                return getChoicesInPlayerQueue();
+            case "InGame":
+                return getChoicesInGame();
+        }
+        String[] x = new String[0];
+        return x;
+    }
 
-		case "InPlayerQueue":
-			return getChoicesInPlayerQueue();
-		case "InGame":
-			return getChoicesInGame();
-		}
-		String[] x = new String[0];
-		return x;
-	}
+    private String[] getChoicesInGame() {
+        String[] response = new String[2];
+        response[0] = ("Sit Down");
+        response[1] = ("Leave Game");
+        return response;
+    }
 
-	private String[] getChoicesInGame() {
-		String[] response = new String[2];
-		response[0] = ("Sit Down");
-		response[1] = ("Leave Game");
-		return response;
-	}
-
-	private String[] getChoicesInPlayerQueue() {
-		String[] response = new String[2];
-		response[0] = ("Force start");
-		response[1] = ("Leave Game");
-		return response;
-	}
-
-
+    private String[] getChoicesInPlayerQueue() {
+        String[] response = new String[2];
+        response[0] = ("Force start");
+        response[1] = ("Leave Game");
+        return response;
+    }
 
 }
