848c5a134105b0e4da2b3763641704025e24865f
Octoberapples <linneamd@gmail.com>
2015-11-21 12:20:23 +0100

    De nyaste ändringarna vi gjorde
    Det  vi gjorde igår

--- musicalChairs/src/musicalChairs/ClientInterface.java
+++ musicalChairs/src/musicalChairs/ClientInterface.java
@@ -5,22 +5,20 @@ import java.util.Scanner;
 public class ClientInterface {
 
     /**
-     * eyeyeyeyye  ändrarrrr
      *
      * @param choices we get from the server
      * @return the client request
      */
-    public static String getRequest(String[] choices) {
+    public static int getRequest( ) {
         int tmp_req;
-        System.out.println("Plz a number between 1 and " + choices.length + ": ");
-        Scanner sc = new Scanner(System.in);
+        		Scanner sc = new Scanner(System.in);
         do {
             CheckIfInt(sc);
             tmp_req = sc.nextInt();
 
-        } while (validNumber(tmp_req, choices.length));
+        } while ( tmp_req == 1|| tmp_req == 2);
 
-        return choices[(tmp_req - 1)];
+        return tmp_req;
     }
 
     /**
@@ -34,16 +32,13 @@ public class ClientInterface {
         }
     }
 
-    private static boolean validNumber(int input, int length) {
-        return (input <= 1 && input >= length);
-    }
-
+ 
     /*
-    Print the requests that are availiblefor the client.
+    Print the requests that are available for the client.
      */
-    public static void printChoices(String[] choices) {
-        for (int i = 0; i < choices.length; i++) {
-            System.out.println(i + ": " + choices[i].toString()); //Kanske måste göra egen toString
+    public static String printChoices(String choices) {
+    	return  "1: " + choices + " \n"
+    			+ "2: Leave Game";
         }
     }
-}
+
