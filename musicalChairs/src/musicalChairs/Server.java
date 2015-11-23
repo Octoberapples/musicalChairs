b72de9fdfc6927c7b9f06f308d3580c06b36b724
nogna <nogna@hotmail.se>
2015-11-23 20:39:10 +0100

    ändringa

--- musicalChairs/src/musicalChairs/Server.java
+++ musicalChairs/src/musicalChairs/Server.java
@@ -5,10 +5,29 @@
  */
 package musicalChairs;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  *
  * @author albin
  */
 public class Server {
-    
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = null;
+        boolean listeningSocket = true;
+        try {
+            serverSocket = new ServerSocket(4242);
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: 4242");
+        }
+        while (listeningSocket) {
+            Socket clientSocket = serverSocket.accept();
+            ServerRun game = new ServerRun(clientSocket);
+            game.start();
+        }
+        serverSocket.close();
+    }
 }
