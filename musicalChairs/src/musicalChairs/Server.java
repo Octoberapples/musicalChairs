9203316152e06a4ffe2007d1a3f51c92165a42bb
nogna <nogna@hotmail.se>
2015-11-26 11:08:30 +0100

    hej

--- musicalChairs/src/musicalChairs/Server.java
+++ musicalChairs/src/musicalChairs/Server.java
@@ -19,7 +19,7 @@ public class Server {
         } catch (IOException e) {
             System.err.println("Could not listen on port: 8080");
         }
-        while (listeningSocket) {
+        while (listeningSocket) { 
             Socket clientSocket = serverSocket.accept();
             ServerRun client = new ServerRun(clientSocket);
             client.start();
