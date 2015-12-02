c4c535ff991e11a581d54aaad229dc4f948b11a9
timkulich <master@student246-168.eduroam.uu.se>
2015-12-02 19:25:21 +0100

    Snygga till lite
    nä

--- musicalChairs/src/musicalChairs/ServerRun.java
+++ musicalChairs/src/musicalChairs/ServerRun.java
@@ -11,22 +11,35 @@ public class ServerRun extends Thread {
 
     private static int PORT; //Kanske onödig
     private ServerSocket SERVERSOCKET;
+    String playerIP;
+    Socket server;
+
 
     public ServerRun(int port) throws IOException {
         super("ServerRun");
         this.SERVERSOCKET = new ServerSocket(port);
         this.PORT = port;
     }
+    
+    public void waitForConnection() throws IOException{
+                System.out.println("Waiting for client on port " + SERVERSOCKET.getLocalPort() + "...");
+                server = SERVERSOCKET.accept();
+                System.out.println("Just connected to " + server.getRemoteSocketAddress());
+                return;
+    }
+    
+    String getIP(Socket server){
+    return server.getRemoteSocketAddress().toString();
+    }
 
     public void run() {
         while (true) {
             try {
-                System.out.println("Waiting for client on port " + SERVERSOCKET.getLocalPort() + "...");
-                Socket server = SERVERSOCKET.accept();
-                System.out.println("Just connected to " + server.getRemoteSocketAddress());
-                DataInputStream in = new DataInputStream(server.getInputStream());
-                System.out.println(in.readUTF()+ " reacieved(?) this from "+ server.getRemoteSocketAddress().toString());
-                DataOutputStream out = new DataOutputStream(server.getOutputStream());
+                waitForConnection();
+                playerIP = getIP(server);
+                DataInputStream in = new DataInputStream(server.getInputStream());//skapar connection in
+                DataOutputStream out = new DataOutputStream(server.getOutputStream());//skapar connection ut
+                System.out.println(in.readUTF()+ " received this from "+ playerIP);
                 out.writeUTF(" de här skickade du till mig"+ "\nGoodbye!");
                 server.close();
 
@@ -39,4 +52,5 @@ public class ServerRun extends Thread {
             }
         }
     }
+    
 }
