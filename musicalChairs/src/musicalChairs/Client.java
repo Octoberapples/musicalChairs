603a25aebc9c8bf5c35e40674406e6db4b64b536
nogna <nogna@hotmail.se>
2015-12-02 20:03:45 +0100

    fix

--- musicalChairs/src/musicalChairs/Client.java
+++ musicalChairs/src/musicalChairs/Client.java
@@ -13,6 +13,7 @@ import java.util.Scanner;
  * @author Albin
  */
 public class Client {
+
     static Socket SOCKET;
     static final String SERVER = "localhost";
     static final int DEFAULT_SOCKET_PORT = 8080; //Kanske vill ha en CommonSTuffClient klass men nog onödigt
@@ -26,11 +27,9 @@ public class Client {
     private static void messageToServer(String clientRequest, long totalResponseTime) throws IOException {
         OutputStream outToServer = SOCKET.getOutputStream();
         DataOutputStream out = new DataOutputStream(outToServer);
-        out.writeUTF(clientRequest+"\n"+totalResponseTime);
+        out.writeUTF(clientRequest + "\n" + totalResponseTime);
     }
-/*
-    bla
-    */
+
     private static String messageFromServer(Socket client) throws IOException {
         InputStream inFromServer = client.getInputStream();
         DataInputStream in = new DataInputStream(inFromServer);
@@ -112,6 +111,7 @@ public class Client {
                 messageToServer(clientInput, totalResponseTime);
             case ("Get Ready"):
                 System.out.println("Nu smäller de snart");
+                messageToServer("READY");
         }
 
     }
