f49780378c8122327384757506e03420b030db2a
timkulich <master@student246-168.eduroam.uu.se>
2015-12-02 19:39:06 +0100

    Revert "Revert "Merge branch 'master' into my_musical_chair_branch""
    This reverts commit d5e3314e9da6617c92b63bb122333da10a27889e.

--- musicalChairs/src/musicalChairs/ClientInterface.java
+++ musicalChairs/src/musicalChairs/ClientInterface.java
@@ -1,32 +1,34 @@
 package musicalChairs;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 /*
-MÅSTE FIXAS, OTRILOGT BUGGIGT
-*/
+
+ MÅSTE FIXAS, OTRILOGT BUGGIGT
+ */
+
 public class ClientInterface {
 
     /**
-     *
-     * @param choices we get from the server
-     * @return the client request
+     *  Fett buggig mannen
+     *  Ge mig SAFE INPUT jag svär jag blir nöjd då
+     * @return the client request in form of a String
      */
-    public static int getRequest() {
-        int tmpRequest;
+    public static String getRequest() {
+
+        System.out.println("1-2: Skriv!");
+        System.out.println(printChoices("Join Game"));
         Scanner sc = new Scanner(System.in);
-        do {
-            CheckIfInt(sc);
-            tmpRequest = sc.nextInt();
-
-        } while (tmpRequest == 1 || tmpRequest == 2);
-
-        return tmpRequest;
+        int tmpRequest = 0;
+        tmpRequest = sc.nextInt();
+        String tmpRequesttoString = Integer.toString(tmpRequest);
+        return tmpRequesttoString;
     }
+
 
     /**
      *
      * @param Input from the player
-     * @param length the length of the choice array
      */
     private static void CheckIfInt(Scanner Input) {
         while (!Input.hasNextInt()) {
@@ -34,12 +36,11 @@ public class ClientInterface {
         }
     }
 
-    
     /**
      * Print the requests that are available for the client.
      */
-    public static String printChoices(String choices) {
-        return "1: " + choices + " \n"
+    public static String printChoices(String choice) {
+        return "1: " + choice + " \n"
                 + "2: Leave Game";
     }
 }
