42b466768dbc5fe819b38168cc5944bcbbcd1a99
nogna <nogna@hotmail.se>
2015-12-02 18:32:36 +0100

    hejehejasjasa

--- musicalChairs/src/musicalChairs/ClientInterface.java
+++ musicalChairs/src/musicalChairs/ClientInterface.java
@@ -2,36 +2,30 @@ package musicalChairs;
 
 import java.util.Scanner;
 /*
-<<<<<<< HEAD
+
  MÅSTE FIXAS, OTRILOGT BUGGIGT
  */
 
-=======
-MÅSTE FIXAS, OTRILOGT BUGGIGT jihihihi
-*/
->>>>>>> origin/master
+
 public class ClientInterface {
 
-<<<<<<< HEAD
     
-    public static int getRequest() {
-=======
+    
     /**
      *
      *
      * @return the client request
      */
     public static String getRequest() {
-<<<<<<< HEAD
+
         System.out.println("1-2: Skriv!");
         System.out.println(printChoices("Hej linnea"));
-=======
->>>>>>> origin/master
+
         int tmpRequest;
         System.out.println("Skriv 1 eller 2");
->>>>>>> origin/master
+
         Scanner sc = new Scanner(System.in);
-        int tmpRequest = sc.nextInt();
+        tmpRequest = sc.nextInt();
         if (sc.hasNextInt() && (tmpRequest == 1 || tmpRequest == 2)) {
             tmpRequest = sc.nextInt();
         } else {
@@ -67,12 +61,6 @@ public class ClientInterface {
 
     /**
      * Print the requests that are available for the client.
-<<<<<<< HEAD
-     *
-     * @param choice
-=======
-     * @param choices
->>>>>>> origin/master
      */
     public static String printChoices(String choice) {
         return "1: " + choice + " \n"
