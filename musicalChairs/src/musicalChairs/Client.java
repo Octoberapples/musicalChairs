6fee0cb2b10ee0db2a8879f4812db9bd32bd9fa9
timkulich <master@masters-MacBook-Air.local>
2015-12-06 13:37:17 +0100

    kommentera bort joinOrExit

--- musicalChairs/src/musicalChairs/Client.java
+++ musicalChairs/src/musicalChairs/Client.java
@@ -68,7 +68,7 @@ public class Client {
         while (true) {
             try {
                 clientSocket = connectToServer();
-                ClientInterface.joinOrExit(clientSocket); //tror jag vill ta in outputstreamen i denna men vet inte hur //för att testa funktionen bara
+                //ClientInterface.joinOrExit(clientSocket); //tror jag vill ta in outputstreamen i denna men vet inte hur //för att testa funktionen bara
                 String clientInput = ClientInterface.getRequest();
                 messageToServer(clientInput);
                 String ServerResponse = messageFromServer(clientSocket);
