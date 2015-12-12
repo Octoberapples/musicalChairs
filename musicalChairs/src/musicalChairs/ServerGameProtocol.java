24b038014d4b088ef2a5248c6000c5d633752986
nogna <nogna@hotmail.se>
2015-12-12 19:24:23 +0100

    tst

--- musicalChairs/src/musicalChairs/ServerGameProtocol.java
+++ musicalChairs/src/musicalChairs/ServerGameProtocol.java
@@ -2,21 +2,26 @@ package musicalChairs;
 
 //Klassen där vi hanterar det vi får från klienten och skickar tillbaka
 //ett serverResponse
-class ServerGameProtocol {
+class ServerGameProtocol extends Thread {
 
     public static Object handleClientInput(Object clientResponse) {
         Object serverResponse = null;
-            if("PLAY".equals(clientResponse)) {
-                System.out.println("What the server does with the clientResponse:'A client wants to play'");
-                serverResponse = "GET READY";
-                       
-            }if("HEJ".equals(clientResponse)) {
-                System.out.println("What the server does with the clientResponse:'A client wants to play'");
-                serverResponse = "SIT DOWN";
-                       
+        if (clientResponse instanceof String) {
+            switch ((String) clientResponse) {
+                case ("PLAY"):
+                    serverResponse = "In Player Queue";
+                    break;
+                case ("GET READY"):
+                    serverResponse = "Music is playing!";
+                    break;
             }
-            
-            return serverResponse;
-    }   
+        }
+        if (clientResponse instanceof Long) {
+            System.out.println("Fick timestampen");
+            serverResponse = "WINNER";
+        }
+
+        return serverResponse;
+    }
 
 }
